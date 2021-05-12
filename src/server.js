8f5d975122729ccb9892eb17388154a8b7857bf3
sree-chikati <srechi453.fusdk12@gmail.com>
2021-05-12 00:10:50 -0700

    editing

--- src/server.js
+++ src/server.js
@@ -6,16 +6,8 @@ const bodyParser = require('body-parser')
 const app = express();
 
 // All app.use
-app.use('*/css',express.static('public/css')); // Allows us to use content from public file
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-
-// Middleware
-const exphbs  = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
 
 app.use((req, res, next) => {
     const now = new Date().toString()
@@ -30,17 +22,10 @@ require("./data/maas-db.js");
 const router = require("./controllers/index.js");
 app.use(router);
 
-// Routes
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.listen(process.env.PORT, () => {
-  console.log(`Maas API listening on port ${process.env.PORT}`);
-});
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log('Maas API listening on port localhost:3000!');
-// })
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Maas API listening on port localhost:3000!');
+})
 
 module.exports = app;
